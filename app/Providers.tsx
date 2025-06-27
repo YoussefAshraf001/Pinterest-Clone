23e6d38ce142ee71e1e7bc90970a4d9172f58273
Youssef Ashraf <youssefashraf273@gmail.com>
2025-06-27 11:20:13 +0300

    Updated Providers code to fix deploy issue

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,9 +1,24 @@
 "use client";
 
+import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 
-function Providers({ children }) {
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+function Providers({ children }: ProvidersProps) {
   return <SessionProvider>{children}</SessionProvider>;
 }
 
 export default Providers;
+
+// "use client";
+
+// import { SessionProvider } from "next-auth/react";
+
+// function Providers({ children }) {
+//   return <SessionProvider>{children}</SessionProvider>;
+// }
+
+// export default Providers;
